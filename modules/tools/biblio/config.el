04027e5a248accbc15c7bd28da68160600d23b40
Bruce D'Arcus <bdarcus@gmail.com>
2021-06-29 13:27:56 -0400

    Add org-cite support to biblio

--- modules/tools/biblio/config.el
+++ modules/tools/biblio/config.el
@@ -12,3 +12,49 @@
   :defer t
   :config
   (add-to-list 'ivy-re-builders-alist '(ivy-bibtex . ivy--regex-plus)))
+
+;;; Org-Cite configuration
+
+(when (featurep! :lang org)
+
+  ;;; Org-cite processors
+  (use-package! oc-basic
+    :after oc)
+
+  (use-package! oc-biblatex
+    :after oc)
+
+  (use-package! oc-csl
+    :after oc
+    :init
+    ;; optional; add to docs instead?
+    (setq org-cite-csl-styles-dir "~/.local/share/csl/styles")
+    (setq org-cite-csl-locales-dir "~/.local/share/csl/locales"))
+
+  (use-package! oc-natbib
+    :after oc)
+
+  (use-package! bibtex-actions-org-cite
+    :when (featurep! :completion vertico)
+    :after (org oc bibtex-actions))
+
+  ;;; Org-cite configuration
+  (use-package! oc
+    :init
+    ;; activate processor for fontification, preview, etc
+    ;; currently using basic, but would prefer org-cite-csl-activate
+    (setq org-cite-activate-processor 'basic)
+
+    (if (featurep! :completion vertico)
+        (setq org-cite-follow-processor 'bibtex-actions-org-cite
+              org-cite-insert-processor 'bibtex-actions-org-cite)
+      (setq org-cite-follow-processor 'basic
+            org-cite-insert-processor 'basic))
+
+    ;; setup export processor; default csl/citeproc-el, with biblatex for latex
+    (setq org-cite-export-processors
+          '((latex biblatex nil nil)
+            (t csl nil nil)))
+
+    ;; need to set oc to use +biblio-default-bibliography-files
+    (setq org-cite-global-bibliography '("~/bib/references.bib"))))
--- modules/tools/biblio/packages.el
+++ modules/tools/biblio/packages.el
@@ -2,6 +2,8 @@
 ;;; tools/biblio/packages.el
 
 (package! bibtex-completion :pin "9f6ea920a49457d85096caa0e61f086a42b2908e")
+(package! citeproc :pin "0857973409e3ef2ef0238714f2ef7ff724230d1c")
+
 (when (featurep! :completion ivy)
   (package! ivy-bibtex :pin "9f6ea920a49457d85096caa0e61f086a42b2908e"))
 (when (featurep! :completion helm)

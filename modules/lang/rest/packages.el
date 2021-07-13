c7b14efd4b8c12c2ee5ea90db04d0579ace664be
A.I <merrick@luois.me>
2021-07-13 22:01:26 +0800

    add back jq-mode pin

--- modules/lang/rest/packages.el
+++ modules/lang/rest/packages.el
@@ -6,7 +6,7 @@
   (package! company-restclient :pin "e5a3ec54edb44776738c13e13e34c85b3085277b"))
 
 (when (featurep! +jq)
-  (package! jq-mode)
+  (package! jq-mode :pin "42ad0a99f0114233e2cb317585cb9af494d18a2f")
   (package! restclient-jq
     :pin "2cc1fd3496f57288de3f97c27a5f018284db2d23"
     :recipe (:host github

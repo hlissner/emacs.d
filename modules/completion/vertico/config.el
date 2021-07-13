e2f321bf0bf882d4e3990d31f65bd5f0fa543c1f
Itai Y. Efrat <itai3397@gmail.com>
2021-07-13 13:55:15 +0300

    vertico: remove `:demand t` from `use-package`'s
    It didn't do anything for the `orderless` statement, and we can setup
    `embark-consult` correctly without it.

--- modules/completion/vertico/config.el
+++ modules/completion/vertico/config.el
@@ -19,7 +19,6 @@
   :after vertico)
 
 (use-package! orderless
-  :demand t
   :config
   (defun +vertico-orderless-dispatch (pattern _index _total)
     (cond
@@ -157,9 +156,8 @@
 
 (use-package! embark-consult
   :after (embark consult)
-  :demand t
-  :hook
-  (embark-collect-mode . consult-preview-at-point-mode))
+  :config
+  (add-hook 'embark-collect-mode-hook #'consult-preview-at-point-mode))
 
 (use-package! wgrep
   :commands wgrep-change-to-wgrep-mode
